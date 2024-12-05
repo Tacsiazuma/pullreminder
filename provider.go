2faac735fccc07379b5e755f3862d666504ad227
Krisztian Papp <krisztian@letscode.hu>
2024-12-05 16:21:34 +0100

    more tests around filtering

--- provider.go
+++ provider.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/google/go-github/v67/github"
 	"golang.org/x/oauth2"
 )
@@ -32,30 +32,58 @@ func (f *FakeProvider) PullRequestsToReturn(repo Repository, token string, prs [
 }
 
 type GithubProvider struct {
+	username string
 }
 
-func NewGithubProvider() *GithubProvider {
-	return &GithubProvider{}
+func NewGithubProvider(username string) *GithubProvider {
+	return &GithubProvider{username: username}
 }
 
 func (f *GithubProvider) GetPullRequests(ctx context.Context, repo Repository, token, base string) ([]*Pullrequest, error) {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	client := github.NewClient(oauth2.NewClient(ctx, ts))
-	options := &github.PullRequestListOptions{Base: base}
+	options := &github.PullRequestListOptions{Base: base, State: "open"}
 	prs, _, err := client.PullRequests.List(ctx, repo.Owner, repo.Name, options)
 	if err != nil {
 		return nil, ErrCannotQueryRepository
 	}
-	return mapToPR(prs), err
+	return f.mapToPR(ctx, client, repo.Owner, repo.Name, prs), err
 }
 
-func mapToPR(origin []*github.PullRequest) (target []*Pullrequest) {
-	target = make([]*Pullrequest, len(origin))
-	for i, pr := range origin {
-		target[i] = &Pullrequest{
+func (f *GithubProvider) mapToPR(ctx context.Context, client *github.Client, owner, name string, origin []*github.PullRequest) (target []*Pullrequest) {
+	target = make([]*Pullrequest, 0)
+	for _, pr := range origin {
+		fmt.Printf("Processing #%d PR\n", *pr.Number)
+		details, _, err := client.PullRequests.Get(ctx, owner, name, *pr.Number)
+		if err != nil {
+			fmt.Printf("Skipping #%d PR due to err %v\n", *pr.Number, err)
+			continue
+		}
+		if *details.State == "close" {
+			fmt.Printf("Skipping #%d PR due to closed state\n", *pr.Number)
+			continue
+		}
+		reviews, _, err := client.PullRequests.ListReviews(ctx, owner, name, *pr.Number, &github.ListOptions{})
+		if err != nil {
+			fmt.Printf("Skipping #%d PR due to err when listing reviews %v\n", *pr.Number, err)
+			continue
+		}
+		var isApprovedByUser bool
+		for _, review := range reviews {
+			fmt.Printf("Review %s in state %s", *review.User.Login, *review.State)
+			if *review.User.Login == f.username && *review.State == "APPROVED" {
+				isApprovedByUser = true
+				break
+			}
+		}
+		if isApprovedByUser {
+			fmt.Printf("Skipping #%d PR due to already being approved\n", *pr.Number)
+			continue
+		}
+		target = append(target, &Pullrequest{
 			Number: *pr.Number,
 			URL:    *pr.HTMLURL,
-		}
+		})
 	}
 	return target
 }
--- provider_test.go
+++ provider_test.go
@@ -14,8 +14,9 @@ func TestProvider(t *testing.T) {
 	}
 	ctx := context.TODO()
 	token := os.Getenv("GITHUB_TOKEN")
+	username := "Tacsiazuma"
 	assert.NotEqual(t, token, "", "A valid github token should be set!")
-	sut := NewGithubProvider()
+	sut := NewGithubProvider(username)
 	t.Run("instance", func(t *testing.T) {
 		t.Run("can be instantiated without a token", func(t *testing.T) {
 			assert.NotNil(t, sut, "should not be nil")
@@ -39,10 +40,23 @@ func TestProvider(t *testing.T) {
 			repo := &Repository{Name: "pullreminder-test", Owner: "tacsiazuma"}
 			prs, err := sut.GetPullRequests(ctx, *repo, token, "main")
 			expected := &Pullrequest{Number: 1, URL: "https://github.com/Tacsiazuma/pullreminder-test/pull/1"}
-			if assert.NotNil(t, prs) {
+			if assert.NotNil(t, prs) && assert.Equal(t, 1, len(prs), "Should contain pull requests") {
 				assert.Equal(t, prs[0], expected, "Should return pull requests")
 				assert.Nil(t, err, "Should not return error")
 			}
+		})
+		t.Run("does not return PRs approved by user", func(t *testing.T) {
+			repo := &Repository{Name: "pullreminder-test", Owner: "tacsiazuma"}
+			prs, err := sut.GetPullRequests(ctx, *repo, token, "approved")
+			assert.Equal(t, 0, len(prs), "Should not return PRs")
+			assert.Nil(t, err, "Should not return error")
+		})
+		// https://github.com/Tacsiazuma/pullreminder-test/pull/3 approved-pr -> closed
+		t.Run("does not return closed PRs", func(t *testing.T) {
+			repo := &Repository{Name: "pullreminder-test", Owner: "tacsiazuma"}
+			prs, err := sut.GetPullRequests(ctx, *repo, token, "closed")
+			assert.Equal(t, 0, len(prs), "Should not return closed PRs")
+			assert.Nil(t, err, "Should not return error")
 		})
 	})
 }
