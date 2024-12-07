6e62b7bd380368ce035fc36d14c9b53b515f27dc
Krisztian Papp <krisztian@letscode.hu>
2024-12-07 11:27:21 +0100

    basic sqlite store implementation

--- store.go
+++ store.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"strings"
+)
+
 type Store interface {
 	AddRepository(repo *Repository) error
 	AddCredentials(provider, token string) error
@@ -12,6 +19,76 @@ type FakeStore struct {
 	credentails  map[string]string
 }
 
+type SqliteStore struct {
+	db *sql.DB
+}
+
+func NewSqliteStore(db *sql.DB) Store {
+	_, err := db.Exec("create table if not exists repositories (name varchar, owner varchar, provider varchar, primary key (name, owner,provider))")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("create table if not exists credentials (provider varchar, token varchar)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &SqliteStore{db: db}
+}
+
+func (s *SqliteStore) AddRepository(repo *Repository) error {
+	_, err := s.db.Exec("insert into repositories (name, owner, provider) values (?,?,?)", repo.Name, repo.Owner, repo.Provider)
+	if err != nil && strings.Contains(err.Error(), "UNIQUE constraint") {
+		return ErrRepositoryDuplicate
+	}
+	return nil
+}
+
+func (s *SqliteStore) AddCredentials(provider, token string) error {
+	_, err := s.db.Exec("insert into credentials (provider, token) values (?,?)", provider, token)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SqliteStore) Credentials() (map[string]string, error) {
+	rows, err := s.db.Query("SELECT provider, token FROM credentials")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	creds := make(map[string]string)
+	for rows.Next() {
+		var token string
+		var provider string
+		err = rows.Scan(&provider, &token)
+		if err != nil {
+			return nil, err
+		}
+		creds[provider] = token
+	}
+	return creds, nil
+}
+
+func (s *SqliteStore) Repositories() ([]*Repository, error) {
+	rows, err := s.db.Query("SELECT name, owner, provider FROM repositories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	repos := make([]*Repository, 0)
+	for rows.Next() {
+		var name string
+		var owner string
+		var provider string
+		err = rows.Scan(&name, &owner, &provider)
+		if err != nil {
+			return nil, err
+		}
+		repos = append(repos, &Repository{Name: name, Owner: owner, Provider: provider})
+	}
+	return repos, nil
+}
 func NewFakeStore() Store {
 	return &FakeStore{repositories: make([]*Repository, 0), credentails: make(map[string]string)}
 }
---
+++ store_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSqliteStore(t *testing.T) {
+	os.Remove("temp.db")
+	db, err := sql.Open("sqlite3", "temp.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sut := NewSqliteStore(db)
+	t.Run("repositories", func(t *testing.T) {
+		t.Run("returns empty slice if no repository added", func(t *testing.T) {
+			expected := make([]*Repository, 0)
+			repos, err := sut.Repositories()
+			assert.Nil(t, err, "Should not return error")
+			assert.Equal(t, expected, repos, "Should return empty slice")
+		})
+		t.Run("can add repository", func(t *testing.T) {
+			err := sut.AddRepository(&Repository{Name: "test", Owner: "me", Provider: "github"})
+			assert.NoError(t, err, "Should not return error")
+		})
+		t.Run("fails to add same repository twice", func(t *testing.T) {
+			err := sut.AddRepository(&Repository{Name: "test", Owner: "me", Provider: "github"})
+			err = sut.AddRepository(&Repository{Name: "test", Owner: "me", Provider: "github"})
+			assert.Equal(t, ErrRepositoryDuplicate, err, "Should return error")
+		})
+		t.Run("returns previously added repositories", func(t *testing.T) {
+			repo := &Repository{Name: "test", Owner: "me", Provider: "github"}
+			sut.AddRepository(repo)
+			repos, err := sut.Repositories()
+			assert.Nil(t, err, "Should not return error")
+			assert.Equal(t, repo, repos[0], "Should return added repos")
+		})
+	})
+	t.Run("credentials", func(t *testing.T) {
+		t.Run("returns empty map if no credentials added", func(t *testing.T) {
+			expected := make(map[string]string)
+			creds, err := sut.Credentials()
+			assert.Nil(t, err, "Should not return error")
+			assert.Equal(t, expected, creds, "Should return empty map")
+		})
+		t.Run("can add credentials", func(t *testing.T) {
+			err := sut.AddCredentials("github", "token")
+			assert.NoError(t, err, "Should not return error")
+		})
+		t.Run("return previously added credentials", func(t *testing.T) {
+			sut.AddCredentials("github", "token")
+			creds, err := sut.Credentials()
+			assert.Nil(t, err, "Should not return error")
+			assert.Equal(t, "token", creds["github"], "Should return token for provider")
+		})
+	})
+}
